4551272d3938693089ccd53b8377f189cad2e4c1
MarcusCaldeira <mac.caldr@gmail.com>
2019-02-27 18:00:49 -0600

    the app is finally starting to come through a little clearer.

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -5,15 +5,11 @@ import logo from './logo.svg';
 // importing the logo 
 import './App.css';
 
-const welcome = "Welcome to React";
+
+
 // Defining a class named app... it will extend component
 class App extends Component {
-  //Render method required for every component. Simply just whats output. 
-  submit = () =>{
-    console.log(this.text.value);
-
-  }
-  
+  //Render method required for every component. Simply just whats output.
   render() {
     return (
       //This is our actual JSX CODE classname instead of class because Class is reserved in js.
@@ -21,32 +17,13 @@ class App extends Component {
         <header className="App-header">
         {/* you can use this js variables */}
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {welcome}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <input type = "text" ref={(input) => this.text = input}/>
-          <button onClick={this.submit}>Show Value</button>
         </header>
       </div>
     );
   }
 }
 
-class Welcome extends Component{
-  render(){
-    const {text} = this.props;
-    return(
-      <h1 className = "app title">{text}</h1>
-    )
-  }
-}
+
+
 
 export default App;
