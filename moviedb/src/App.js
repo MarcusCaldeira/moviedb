e52bcc3253e34b8246521b998461b23dbfcf62c3
MarcusCaldeira <mac.caldr@gmail.com>
2019-02-28 14:35:35 -0600

    making changes this is kinda hard.

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -4,30 +4,33 @@ import './App.css';
 
 import Movie from './movie';
 
-const movies = [{
-    id: 1,
-    title: 'Star Wars'
-  }, {
-    id: 2,
-    title: 'Spider Man'
-  }, {
-    id: 3,
-    title: '36th Chamber of Shaolin'
-  }, {
-    id: 4,
-    title: '5 Deadly Venoms'
-  }
-];
 
 
 class App extends Component {
+state = {
+  movies: []
+}
+
+  async componentDidMount(){
+    try {
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=15123773d1c1297ddb32ef458dd99384&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
+      const movies = await res.json();
+      console.log(movies);
+      this.setState({
+        movie: movies.results
+      })
+    } catch(e){
+      console.log(e);
+    }
+
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => <Movie key={movie.id} movie={movie} />)}
+        {this.state.movies.map(movie => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
