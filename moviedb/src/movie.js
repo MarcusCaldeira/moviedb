1924da603821c3960fbba15a117234a4a9edb422
MarcusCaldeira <mac.caldr@gmail.com>
2019-03-02 14:34:32 -0600

    ok now we got some movie posters, not really sure what I was doing that was so different than the instructional video.

--- moviedb/src/movie.js
+++ moviedb/src/movie.js
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-const = Movie = () => {
-    return (
-      <div>
-        <h3>{this.props.movie.title}</h3>
-      </div>
-    );
-}
-// static propTypes = {
-//     movie: PropTypes.shape({
-//         title: PropTypes.string.isRequired,
-//     }),
-// }
+const POSTER_PATH = 'http://image.tmdb.org/t/p/w154';
+
+const Movie = ({ movie }) => (
+  <div>
+    <img src={`${POSTER_PATH}${movie.poster_path}`} alt={movie.title} />
+  </div>
+);
+
+export default Movie;
+
+Movie.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+  }).isRequired,
+};
